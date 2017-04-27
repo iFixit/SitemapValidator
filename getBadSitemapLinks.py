fee91fff24c26865ed9452ce75e9ac4575d0af46
Chris Opperwall <chriso@ifixit.com>
2017-04-26 19:13:14 -0700

    SitemapValidator: Add tool to record sitemap inconsistencies
    This adds a new tool that pulls down all sitemap XML documents and finds any
    links that either respond with a non 200 code or have a different canonical url
    than the sitemap entry.

---
+++ getBadSitemapLinks.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+# This script takes a sitemap XML filename and visits the URL from each <loc>
+# tag.
+#
+# It records the sitemap URL, canonical URL, and response code from each URL
+# GET and writes any responses that have a bad response code or
+# sitemap/canonical URL mismatch to a JSON file.
+
+import sys
+import re
+import json
+import xml.etree.ElementTree as ET
+
+from urllib.request import urlopen
+from urllib.error import HTTPError
+
+from multiprocessing import Pool
+
+def get_urls_from_file(filename):
+    """Get a list of all URLs from the given XML file"""
+    tree = ET.parse(filename)
+    namespaces = {'sitemap': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
+
+    root = tree.getroot()
+    return list(map(lambda x: x.text,
+        root.findall('./sitemap:url/sitemap:loc', namespaces)))
+
+def grab_canonical_from_html(html):
+    """Extract the canonical URL from the given HTML document"""
+    matches = re.search('rel="canonical" href="([^"]+)"', html)
+    return matches.group(1)
+
+def map_url_status(sitemap_url):
+    """Map the given sitemap_url into a dictionary of the format
+       {'sitemap_url': '...',
+        'canonical_url': '...',
+        'code': xxx}
+    """
+    try:
+        with urlopen(sitemap_url) as res:
+            html = res.read().decode('utf-8')
+            return {'sitemap_url': sitemap_url,
+                    'canonical_url': grab_canonical_from_html(html),
+                    'code': res.code}
+    except HTTPError as e:
+        print('Error!', e)
+        return {'sitemap_url': sitemap_url,
+                'canonical_url': '',
+                'code': e.code}
+
+def has_bad_response_code(status):
+    return status['code'] != 200
+
+def has_url_mismatch(status):
+    success_code = status['code'] == 200
+    bad_canonical = status['sitemap_url'] != status['canonical_url']
+
+    return success_code and bad_canonical
+
+# For each URL in the sitemap file, return a
+#   sitemap_url
+#   canonical_url
+#   code
+#
+# Parallelize this because we can.
+# NOTE: If you run this from outside of the office you WILL get rate limited.
+def get_bad_links(urls):
+    with Pool(20) as pool:
+        statuses = pool.map(map_url_status, urls)
+
+    # Filter statuses into lists of bad response codes and url mismatches
+    bad_code_statuses = list(filter(has_bad_response_code, statuses))
+    url_mismatch_statuses = list(filter(has_url_mismatch, statuses))
+
+    # Grab the filename without the .xml extension
+    outfile_name = re.search('([a-z0-9_]+)\.xml$', filename).group(1)
+
+    # Write out the list of bad code statuses and url mismatch statuses to their
+    # own respective JSON files.
+    with open(outfile_name + '-bad-code.json', 'w') as out:
+        json.dump(bad_code_statuses, out)
+
+    with open(outfile_name + '-url-mismatch.json', 'w') as out:
+        json.dump(url_mismatch_statuses, out)
+
+# This file can be run as a standalone script, or as a module.
+# When run as a script, take a filename argument.
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./get_bad_sitemap_links.py <sitemap xml file>")
+        exit()
+
+    filename = sys.argv[1]
+    urls = get_urls_from_file(filename)
+    get_statuses(urls)
---
+++ sitemapValidator.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# This gets the links of all sitemaps from ifixit.com/sitemap.xml, downloads
+# them, and then checks each link response for a 200 code and a matching
+# canonical url in the HTML.
+
+from urllib.request import urlopen, urlretrieve
+import xml.etree.ElementTree as ET
+import re
+
+import getBadSitemapLinks
+
+def get_sitemap(url):
+    """Pull down the sitemap XML file at the given url and save to a file"""
+    outfile = get_filename_from_sitemap(url)
+    print('Downloading ' + outfile + '...')
+
+    urlretrieve(url, outfile)
+
+def get_filename_from_sitemap(sitemap_url):
+    """Extract the filename from the sitemap url"""
+    matches = re.search('\/([a-z0-9_]+\.xml)$', sitemap_url)
+
+    if matches:
+        return matches.group(1)
+    return None
+
+sitemap_index_url = 'https://www.ifixit.com/sitemap.xml'
+
+with urlopen(sitemap_index_url) as res:
+    xml = res.read().decode('utf-8')
+
+tree = ET.fromstring(xml)
+namespaces = {'sitemap': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
+
+sitemap_urls = list(map(lambda url_element: url_element.text,
+        tree.findall("./sitemap:sitemap/sitemap:loc", namespaces)))
+
+# Download sitemap files
+for url in sitemap_urls:
+    get_sitemap(url)
+
+# Get file names from urls
+sitemap_filenames = list(map(get_filename_from_sitemap, sitemap_urls))
+
+# Find all urls in the sitemap XML document and write any bad links to file.
+for filename in sitemap_filenames:
+    print("Getting bad links for " + filename + " sitemap")
+    urls = getBadSitemapLinks.get_urls_from_file(filename)
+    getBadSitemapLinks.get_bad_links(urls)
